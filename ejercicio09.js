1d5086e505b0ce9748b0a6c1347a96041f6eb634
JonahtanMG <jmamanigut@unsa.edu.pe>
2025-12-01 11:14:11 -0500

    Ejercicio 09

---
+++ ejercicio09.js
@@ -0,0 +1,17 @@
+const pokemon = document.getElementById("buscar");
+const boton = document.querySelector("button");
+const div = document.querySelector("div");
+
+boton.addEventListener("click", () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon.value)
+        .then(res => res.json())
+        .then(data => {
+            const tipo = data.types.map(t=>t.type.name).join(", ");
+            div.innerHTML = `
+            <p>Nombre: ${data.name}</p>
+            <img src="${data.sprites.front_default}" alt="${data.name}">
+            <p>Tipos: ${tipo}</p>
+        `
+        })
+        .catch(err => console.error("Error:", err));
+})
