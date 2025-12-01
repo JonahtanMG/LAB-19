3e6e8dce9a4b6ee27f81e114c8e8dd43daac8693
JonahtanMG <jmamanigut@unsa.edu.pe>
2025-12-01 11:51:54 -0500

    Ejercicio 10

--- ejercicio10.js
+++ ejercicio10.js
@@ -1,22 +1,18 @@
-const pokemon = document.getElementById("buscar");
-const boton = document.querySelector("button");
 const div = document.querySelector("div");
 
-boton.addEventListener("click", () => {
-    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon.value)
-        .then(res => res.json())
-        .then(data => {
-            const statsHTML = data.stats.map(stat => {
-                return `<li>${stat.stat.name}: ${stat.base_stat}</li>`;
-            }).join("");
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(res => res.json())
+    .then(data => {
+        const statsHTML = data.stats.map(stat => {
+            return `<p>${stat.stat.name}: ${stat.base_stat}</p>`;
+        }).join("");
 
-            const tipo = data.types.map(t => t.type.name).join(", ");
-            div.innerHTML = `
+        const tipo = data.types.map(t => t.type.name).join(", ");
+        div.innerHTML = `
             <p>Nombre: ${data.name}</p>
             <img src="${data.sprites.front_default}" alt="${data.name}">
             <p>Tipos: ${tipo}</p>
-            <ul>${statsHTML}</ul>
+            <p>${statsHTML}</p>
         `
-        })
-        .catch(err => console.error("Error:", err));
-})
+    })
+    .catch(err => console.error("Error:", err));
